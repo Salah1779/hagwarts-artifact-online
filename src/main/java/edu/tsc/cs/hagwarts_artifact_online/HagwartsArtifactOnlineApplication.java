c3ba867cac6c37ec281ff77c6db424cc5086b6d8
Salah1779 <salahbusiness18@gmail.com>
2024-12-19 01:44:35 +0100

    this is the first commit.

---
+++ src/main/java/edu/tsc/cs/hagwarts_artifact_online/HagwartsArtifactOnlineApplication.java
@@ -0,0 +1,13 @@
+package edu.tsc.cs.hagwarts_artifact_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HagwartsArtifactOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HagwartsArtifactOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/tsc/cs/hagwarts_artifact_online/HagwartsArtifactOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tsc.cs.hagwarts_artifact_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HagwartsArtifactOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
